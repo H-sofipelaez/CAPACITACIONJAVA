06a1acca77e360b3d18377c16f72de36e081b314
“spelaez” <spelaezarias@gmail.com>
2025-05-26 14:38:32 -0500

    Ejercicio de juego de recursividad

---
+++ Juego_Recursividad/src/main/java/org/example/Adivinanza.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Adivinanza {
+    public static void main (String [] args) {
+        Scanner leer = new Scanner(System.in);
+        Random aleatorio = new Random();
+        int numeroSecreto = aleatorio.nextInt(10) + 1;
+        System.out.println("Bienvenido al juego de adivinanza");
+        System.out.println("Adivina un número entre 1 y 10");
+
+        adivinar(leer, numeroSecreto);
+        leer.close();
+    }
+
+    public static void adivinar(Scanner scanner, int secreto) {
+        System.out.print("Ingresa tu número: ");
+        int intento = scanner.nextInt();
+
+        if (intento == secreto) {
+            System.out.println("¡Correcto! Has adivinado el número.");
+        } else {
+            if (intento < secreto) {
+                System.out.println("El número es mayor.");
+            } else {
+                System.out.println("El número es menor.");
+            }
+            adivinar(scanner, secreto); // Llamada recursiva
+        }
+    }
+}
