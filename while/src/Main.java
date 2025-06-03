f01b469f94931224904a432d0998bff3e3dedda5
“spelaez” <spelaezarias@gmail.com>
2025-06-03 14:39:25 -0500

    Primer ejercicio de puble con incremento, incrementando el valor desde cero, en la condiicion dice que debe ser menor a 10, cuando es menor a 10 el ciclo para

---
+++ while/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    // Bucle While
+        int i = 0; // iterador, el valor inicial del bucle
+
+        //inicializar el bucle
+        while (i<10) {
+            System.out.println("El valor es:" + i); //Concatenar, juntar la frase con el valor que tenga la variable
+            i++; //Incremento
+        }
+    }
+}
