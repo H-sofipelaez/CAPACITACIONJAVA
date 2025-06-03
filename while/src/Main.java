18c817fa6c004f194e23a009d57a508ae880b800
“spelaez” <spelaezarias@gmail.com>
2025-06-03 14:43:45 -0500

    Primer ejercicio de puble con decremento, en vez de sumarlo como en el incremento en este caso se resta osea debe ser mayor a 0

--- while/src/Main.java
+++ while/src/Main.java
@@ -3,12 +3,12 @@
 public class Main {
     public static void main(String[] args) {
     // Bucle While
-        int i = 0; // iterador, el valor inicial del bucle
+        int i = 10; // iterador, el valor inicial del bucle
 
         //inicializar el bucle
-        while (i<10) {
+        while (i > 0) {
             System.out.println("El valor es:" + i); //Concatenar, juntar la frase con el valor que tenga la variable
-            i++; //Incremento
+            i--; //Decremento
         }
     }
 }
