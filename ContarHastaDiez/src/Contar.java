73528db3d85f868bdf652e6c4397a0d0348b3fa9
“spelaez” <spelaezarias@gmail.com>
2025-06-05 11:01:32 -0500

    Se realizaron unas mejoras con respecto al ejercicio de suma y detener con while

--- ContarHastaDiez/src/Contar.java
+++ ContarHastaDiez/src/Contar.java
@@ -4,19 +4,14 @@ public class Contar {
     public static void main(String[] args) {
         Scanner contar = new Scanner(System.in);
 
-        int numero;
+        int numero = 1;
         int suma = 0;
 
         System.out.println("Escribe números para sumar. Escribe 0 para terminar.");
 
-        while (true) {
+        while (numero != 0) {
             System.out.print("Número: ");
             numero = contar.nextInt();
-
-            if (numero == 0) {
-                break;
-            }
-
             suma += numero;
         }
 
