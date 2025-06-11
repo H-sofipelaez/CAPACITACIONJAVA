c7fe726cafb91654b676f1dabf58d188fa0e8909
“spelaez” <spelaezarias@gmail.com>
2025-06-11 15:23:54 -0500

    Ejercicio de pares con ciclos

--- ciclo/src/ciclo.java
+++ ciclo/src/ciclo.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class ciclo {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int n;
-        int c = 0;
+        int n,p;
+        int c = 1;
         System.out.println("Ingrese un numero: ");
         n = entrada.nextInt();
-        while (c < n) {
-            System.out.println(c+".");
-            c++;
 
+        while (c <= n) {
+            p = 2*c-2;
+            System.out.println(p+".");
+            c++;
         }
 
     }
